56368e7b0a95ae3aff5ec5d3c121bb856cd34f5d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-04-16 13:35:18 +0300

    Notification Number

--- src/Components/Header/HeaderPresenter.js
+++ src/Components/Header/HeaderPresenter.js
@@ -24,10 +24,24 @@ const HeaderIcon = styled.span`
   cursor: pointer;
   box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
   transition: all 0.2s ease-out;
+  position: relative;
   &:hover {
     transform: translateY(-1px);
     box-shadow: 0 7px 14px rgba(50, 50, 93, 0.1), 0 3px 6px rgba(0, 0, 0, 0.08);
   }
+`;
+
+const Number = styled.span`
+  height: 30px;
+  width: 30px;
+  border-radius: 50%;
+  background-color: #8e44ad;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  left: 25px;
+  top: -10px;
 `;
 
 const HeaderPresenter = ({}) => (
@@ -46,6 +60,7 @@ const HeaderPresenter = ({}) => (
           </HeaderIcon>
           <HeaderIcon>
             <FontAwesome name="bell" />
+            <Number>3</Number>
           </HeaderIcon>
         </Flex>
       </FlexItem>
