c5d4974cacf8a2efd7fea8fbd865ae38dc8b6c0f
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-04-16 14:04:14 +0300

    Store Actions

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -16,11 +16,30 @@ class AppContainer extends Component {
         text: "Wasup!",
         seen: false
       }
-    }
+    },
+    deleteNotification: this._deleteNotification,
+    seeNotification: this._seeNotification
   };
   render() {
     return <AppPresenter {...this.state} />;
   }
+  _deleteNotification = id => {
+    this.setState(prevState => {
+      const newState = delete prevState[id];
+      return newState;
+    });
+  };
+  _seeNotification = id => {
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        [id]: {
+          ...prevState[id],
+          seen: true
+        }
+      };
+    });
+  };
 }
 
 export default AppContainer;
